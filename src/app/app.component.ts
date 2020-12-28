cf019404485934081e99464fc6bdcbc0e6157478
Mohamed Zitouni <zitouni.mohamed.email@gmail.com>
2020-12-28 12:41:39 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as allNodes from '../assets/nodes.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myUdemyApp';
+export class AppComponent implements OnInit{
+  nodes = (allNodes as any).default;
+
+  options = {};
+
+  ngOnInit(){
+    console.log(this.nodes);
+  }
+  
+  onEvent($event){
+    console.log($event);
+  }
 }
